a5aa19eb91873681713d2c08bd4c95e08a549885
holddie <holddie@163.com>
2017-11-30 21:30:58 +0800

    完成git同步操作

--- client/src/main/java/com/holddie/cloud/config/client/ClientApplication.java
+++ client/src/main/java/com/holddie/cloud/config/client/ClientApplication.java
@@ -2,11 +2,14 @@ package com.holddie.cloud.config.client;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
 
 @SpringBootApplication
+@EnableConfigurationProperties
 public class ClientApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ClientApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ClientApplication.class, args);
+    }
 }
---
+++ client/src/main/java/com/holddie/cloud/config/client/controller/HelloController.java
@@ -0,0 +1,24 @@
+package com.holddie.cloud.config.client.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 配置文件访问
+ * @author HoldDie
+ * @version 1.0.0
+ * @email holddie@163.com
+ * @date 2017/11/30 20:49
+ */
+@RestController
+public class HelloController {
+
+    @Value("${holddie.hello}")
+    private  String  hello;
+
+    @RequestMapping(value = "/hello")
+    public String from(){
+        return this.hello;
+    }
+}
